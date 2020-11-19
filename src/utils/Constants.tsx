6eb3e4623cfec34d944a7bd6dae230e4ea84942e
patneparth <51788948+patneparth@users.noreply.github.com>
2020-11-19 20:36:59 +0530

    Updated Checklist Template (#5)
    Co-authored-by: Ashish Shrivastava <asshriv@microsoft.com>

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -5,7 +5,8 @@ export class Constants {
     // ASCII value for carriage return
     public static readonly CARRIAGE_RETURN_ASCII_VALUE = 13;
 
-    public static readonly ACTION_INSTANCE_INDEFINITE_EXPIRY = -1;
+    //Set soft expiry for each actionInstance as 30 days after its creation time.
+    public static readonly ACTION_INSTANCE_DEFAULT_EXPIRY = Date.now() + (30 * 24 * 60 * 60 * 1000);
 
     // some OS doesn't support long filenames, so capping the action's title length to this number
     public static readonly ACTION_RESULT_FILE_NAME_MAX_LENGTH: number = 50;
